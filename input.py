04e17c1c17bf31abb23f1585f3ce10b25f31568c
Ba'tiar Afas Rahmamulia <afasari11@gmail.com>
2017-07-10 04:40:52 +0700

    final commit
    fix bug

---
+++ input.py
@@ -0,0 +1,14 @@
+n = 29
+i = 1
+a = 301
+
+for i in range(n, a):
+	if i%7 == 0 and i%13 == 0:
+		print ('abc')
+	elif i%7 == 0:
+		print ('xyz')
+	elif i%13 == 0:
+		print ('a-z')
+	else:
+		print (i)
+		
